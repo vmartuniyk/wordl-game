c3cab543c14a9a0ad37776c5217e9954735f6403
vmartuniyk <martuniyk93@ukr.net>
2023-02-20 10:47:13 +0200

    Change url for assets

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <script src="//unpkg.com/alpinejs" defer></script>
-    <script src="/js/app.js"></script>
+    <script src="{{ mix('js/app.js') }}"></script>
     <title>Try Cat</title>
 </head>
 <body>
