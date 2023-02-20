13d73ea1843dbf04aa62de20807b04a719a0b202
vmartuniyk <martuniyk93@ukr.net>
2023-02-20 11:48:10 +0200

    Add some style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,14 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/app.css">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-    <script src="{{ mix('js/app.js') }}"></script>
+    <script src="js/app.js"></script>
     <title>Try Cat</title>
 </head>
 <body>
@@ -15,9 +20,12 @@
         x-data="game"
         @keyup.window="onKeyPress($event.key)"
     >
+        <h1 aria-label="TryCat">
+            <img src="/images/trycat-logo.svg" alt="Try Cat">
+        </h1>
         <div id="game">
-            <template x-for="row in board">
-                <div class="row">
+            <template x-for="(row,index) in board">
+                <div class="row" :class="{'current' : currentRowIndex === index}">
                     <template x-for="tile in row">
                         <div class="tile" :class="tile.status" x-text="tile.letter"></div>
                     </template>
